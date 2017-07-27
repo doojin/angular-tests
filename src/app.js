598de7a35922fa6c09e6215e29837f1595e7eb92
doojin <dmitry.papka@gmail.com>
2017-07-27 21:45:26 +0100

    "Hello world" with babel + webpack

---
+++ src/app.js
@@ -0,0 +1,8 @@
+class A {
+    x() {
+        console.log('x');
+    }
+}
+
+let a = new A();
+a.x();
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+};
